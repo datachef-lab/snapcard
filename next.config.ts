6cf5293b1a5929a4d8afafef9ae3eee152bbfc87
desaiHarsh08 <desaiharsh183@gmail.com>
2025-07-18 20:13:55 +0530

    updated paths

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    basePath: "/besc/id-card-generate"
+    // basePath: "/besc/id-card-generate",
+    async rewrites() {
+        return [
+            {
+                source: "/besc/id-card-generate/:path*",
+                destination: "/:path*",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
